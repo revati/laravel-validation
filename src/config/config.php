6e91f9b2d105469752478542b8535f239f4e184b
revati <roberts@rego.lv>
2014-02-01 15:20:59 +0200

    Fixed typo

--- src/config/config.php
+++ src/config/config.php
@@ -11,7 +11,7 @@ return array(
     |
     */
 
-    'throwException' => true;
+    'throwException' => true,
 
     /*
     |--------------------------------------------------------------------------
